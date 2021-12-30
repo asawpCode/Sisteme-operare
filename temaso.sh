53addb80593a83bca2a69dbd58474beb55abb3ea
asawpCode <61597843+asawpCode@users.noreply.github.com>
2021-12-30 16:55:29 +0200

    Add files via upload

--- temaso.sh
+++ temaso.sh
@@ -7,7 +7,7 @@ cap2 () {
   select opt in "${options[@]}"
   do
       case $opt in
-          "Comanda ")
+          "Comanda 1")
               echo "ps"            
               ps
               ;;
@@ -65,7 +65,7 @@ cap3 () {
   select opt in "${options[@]}"
   do
       case $opt in
-          "Prima comanda")
+          "Comanda 1")
               echo "cat /etc/paswwd"
               cat /etc/paswwd
               ;;
@@ -130,7 +130,7 @@ cap4 () {
   select opt in "${options[@]}"
   do
       case $opt in
-          "Prima comanda")
+          "Comanda 1")
               echo "echo $PATH"
               echo $PATH
               ;;
@@ -190,7 +190,7 @@ cap5 () {
   select opt in "${options[@]}"
   do
       case $opt in
-          "Prima comanda")
+          "Comanda 1")
               echo "ps -e"
               ps -e
               ;;
@@ -219,24 +219,16 @@ cap5 () {
               jobs
               ;;
            "Comanda 8")
-              echo "cat /etc/apt/sources.list"
-              cat /etc/apt/sources.list
+              echo "fg"
+              fg
               ;;
            "Comanda 9")
-              echo "apt-get update"
-              apt-get update
+              echo "kil -1"
+              kill -1
               ;;
            "Comanda 10")
-              echo "apt-cache search hevea"
-              apt-cache search hevea
-              ;;
-           "Comanda 9")
-              echo "apt-get install apt-file"
-              apt-get install apt-file
-              ;;
-           "Comanda 9")
-              echo "apt-get update"
-              apt-get update
+              echo "free"
+              free
               ;;
             "Exit")
               return
@@ -247,60 +239,28 @@ cap5 () {
       esac
   done
 }
-cap2 () {
+cap6 () {
   local PS3='Selectati comanda: '
-  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Comanda 5" "Comanda 6" "Comanda 7" "Comanda 8" "Comanda 9" "Comanda 10" "Exit")
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Exit")
   local opt
   select opt in "${options[@]}"
   do
       case $opt in
-          "Prima comanda")
-              echo "cat /etc/paswwd"
-              cat /etc/paswwd
+          "Comanda 1")
+              echo "runlevel"
+              runlevel
               ;;
           "Comanda 2")
-              echo "ls -1 /etc/shadow"
-              ls -1 /etc/shadow
+              echo "init 0"
+              whatis init1
               ;;
           "Comanda 3")
-              echo "passwd guest"
-              passwd guest
+              echo "shutdown -h"
+              info shutdown -h
               ;;
           "Comanda 4")
-              echo "adduser uso"
-              adduser uso
-              ;;
-           "Comanda 5")
-              echo "addgroup usogroup"
-              addgroup usogroup
-              ;;
-           "Comanda 6")
-              echo "cd -"
-              cd -
-              ;;
-           "Comanda 7")
-              echo "dpkg -L coreutils"
-              dpkg -L coreutils
-              ;;
-           "Comanda 8")
-              echo "cat /etc/apt/sources.list"
-              cat /etc/apt/sources.list
-              ;;
-           "Comanda 9")
-              echo "apt-get update"
-              apt-get update
-              ;;
-           "Comanda 10")
-              echo "apt-cache search hevea"
-              apt-cache search hevea
-              ;;
-           "Comanda 9")
-              echo "apt-get install apt-file"
-              apt-get install apt-file
-              ;;
-           "Comanda 9")
-              echo "apt-get update"
-              apt-get update
+              echo "grub"
+              grub
               ;;
             "Exit")
               return
@@ -312,28 +272,238 @@ cap2 () {
   done
 }
 
+cap7 () {
+  local PS3='Selectati comanda: '
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Exit")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Comanda 1")
+              echo "lsmod"
+              lsmod
+              ;;
+          "Comanda 2")
+              echo "cat /proc/modules"
+              cat /proc/modules
+              ;;
+          "Comanda 3")
+              echo "lspci"
+              lspci
+              ;;
+          "Comanda 4")
+              echo "hwinfo --short"
+              hwinfo --short
+              ;;
+           "Comanda 5")
+              echo "sensors"
+              sensors
+              ;;
+           "Comanda 6")
+              echo "hwinfo --short"
+              hwinfo --short
+              ;;
+           "Comanda 7")
+              echo "cat /proc/interrupts"
+              cat /proc/interrupts
+              ;;
+           "Comanda 8")
+              echo "cat /proc/meminfo"
+              cat /proc/meminfo
+              ;;                            
+            "Exit")
+              return
+              ;;
+
+ 
+          *) echo "Optiune invalida $REPLY";;
+      esac
+  done
+}
+
+cap8 () {
+  local PS3='Selectati comanda: '
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Comanda 5" "Comanda 6" "Exit")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Comanda 1")
+              echo "ipconfig"
+              ipconfig
+              ;;
+          "Comanda 2")
+              echo "route"
+              route
+              ;;
+          "Comanda 3")
+              echo "dhclient"
+              dhclient
+              ;;
+          "Comanda 4")
+              echo "hostname"
+              hostname
+              ;;
+           "Comanda 5")
+              echo "ping 10.1.1.1"
+              ping 10.1.1.1
+              ;;
+           "Comanda 6")
+              echo "traceroute rol.ro"
+              traceroute rol.ro
+              ;;                         
+            "Exit")
+              return
+              ;;
+
+ 
+          *) echo "Optiune invalida $REPLY";;
+      esac
+  done
+}
+
+cap9 () {
+  local PS3='Selectati comanda: '
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Exit")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Comanda 1")
+              echo "notstat --tcp --listening"
+              notstat --tcp --listening
+              ;;
+          "Comanda 2")
+              echo "telnet reddit.com"
+              telnet reddit.com
+              ;;
+          "Comanda 3")
+              echo "hostname; pwd"
+              hostname; pwd
+              ;;
+          "Comanda 4")
+              echo "telnet google.ro 80"
+              telnet google.ro 80
+              ;;                         
+            "Exit")
+              return
+              ;;
+
+ 
+          *) echo "Optiune invalida $REPLY";;
+      esac
+  done
+}
+
+cap10 () {
+  local PS3='Selectati comanda: '
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Comanda 5" "Comanda 6" "Comanda 7" "Comanda 8" "Exit")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Comanda 1")
+              echo "passwd"
+              passwd
+              ;;
+          "Comanda 2")
+              echo "pwgen -N 1"
+              pwgen -N 1
+              ;;
+          "Comanda 3")
+              echo "unmask"
+              unmask
+              ;;
+          "Comanda 4")
+              echo "df -h"
+              df -h
+              ;;     
+          "Comanda 5")
+              echo "uptime"
+              uptime
+              ;;   
+          "Comanda 6")
+              echo "ulimit -a"
+              ulimit -a
+              ;;   
+           "Comanda 7")
+              echo "tcpdump"
+              tcpdump
+              ;; 
+           "Comanda 8")
+              echo "who"
+              who
+              ;;
+            "Exit")
+              return
+              ;;    
+
+ 
+          *) echo "Optiune invalida $REPLY";;
+      esac
+  done
+}
+
+cap12 () {
+  local PS3='Selectati comanda: '
+  local options=("Comanda 1" "Comanda 2" "Comanda 3" "Comanda 4" "Exit")
+  local opt
+  select opt in "${options[@]}"
+  do
+      case $opt in
+          "Comanda 1")
+              echo "Hello, World!"
+              ;;
+          "Comanda 2")
+              echo "#comentariu"
+              ;;
+            "Exit")
+              return
+              ;;
+          *) echo "Optiune invalida $REPLY";;
+      esac
+  done
+}
 
 # main menu
 PS3='Please enter main option: '
-options=("Capitolul 3" "Capitolul 4" "Main menu quit")
+options=("Capitolul 2" "Capitolul 3" "Capitolul 4" "Capitolul 5" "Capitolul 6" "Capitolul 7" "Capitolul 8" "Capitolul 9" "Capitolul 10" "Capitolul 12" "Main menu quit")
 select opt in "${options[@]}"
 do
     case $opt in
         "Capitolul 2")
-            cap1
-            ;;
-        "Capitolul 3")
             cap2
             ;;
-        "Capitolul 4")
+        "Capitolul 3")
             cap3
             ;;
-        "Capitolul 5")
+        "Capitolul 4")
             cap4
+            ;;
+        "Capitolul 5")
+            cap5
             ;;
         "Capitolul 6")
-            cap4
+            cap6
             ;;
+        "Capitolul 7")
+            cap7
+            ;;
+        "Capitolul 8")
+            cap8
+            ;;
+        "Capitolul 9")
+            cap9
+            ;;
+        "Capitolul 10")
+            cap10
+            ;;
+         "Capitolul 11")
+            cap11
+            ;;
+        "Capitolul 12")
+            cap12
+            ;;                                   
         "Main menu quit")
             exit
             ;;
